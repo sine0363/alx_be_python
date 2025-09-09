ae03cb6638a53958130ab37f189d4871ba3484f7
Sinethemba Zulu <sinethemba0363@gmail.com>
2025-09-09 12:16:55 +0200

    changes in variable declared

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,10 +1,6 @@
-P = 1000
-R= 0.05
-T = 3
+principal = 1000
+rate = 0.05
+time = 3
 
-# ( I ) is the interest earned,
-# ( P ) is the principal amount (initial investment),
-# ( R ) is the annual interest rate (as a decimal),
-# ( T ) is the time the money is invested for in years.
-I = P* R * T
-print("The simple interest is:", I)
+interest = principal * rate * time
+print("The simple interest is:", interest)
