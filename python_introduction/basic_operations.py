fa18cdb700bd29580308e1cf2b067c79cf0dce0a
Sinethemba Zulu <sinethemba0363@gmail.com>
2025-09-09 11:47:13 +0200

    operators

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,5 +1,8 @@
 number1=10
 number2=5
-
 sum =number1 + number2
+sum =number1 - number2
+sum =number1 * number2
 print("Addition of", number1, "and", number2, "is", sum)
+print("Subtraction of", number1, "and", number2, "is", sum)
+print("Multiplication of", number1, "and", number2, "is", sum)
