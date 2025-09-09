18aeb45c841f09c2b3daf3c217aee62e5dab5b1e
Sinethemba Zulu <sinethemba0363@gmail.com>
2025-09-09 11:32:26 +0200

    subsration operation fixed

--- basic_operations.py
+++
@@ -1,7 +0,0 @@
-number1=10
-number2=5
-
-sum =number1 + number2
-print("Addition of", number1, "and", number2, "is", sum)
-
-
---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,5 @@
+number1=10
+number2=5
+
+sum =number1 + number2
+print("Addition of", number1, "and", number2, "is", sum)
