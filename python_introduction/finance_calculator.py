98b2a7803085f1ec0ffe1e724e012434325b019c
Sinethemba Zulu <sinethemba0363@gmail.com>
2025-09-09 15:15:30 +0200

    personal calculator

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,7 +1,7 @@
-users_income = float(input("Enter your monthly income: "))
-users_expenses = float(input("Enter your total monthly expenses: "))
+monthly_income = float(input("Enter your monthly income: "))
+monthly_expenses = float(input("Enter your total monthly expenses: "))
 
-monthly_savings = users_income-users_expenses
+monthly_savings = monthly_income - monthly_expenses
 
 projected_savings = monthly_savings  * 12 + (monthly_savings* 12 * 0.05)
 
