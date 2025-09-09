b899428ca5f526d8246064ae235981de960da50f
Sinethemba Zulu <sinethemba0363@gmail.com>
2025-09-09 11:59:25 +0200

    changes in variable assignment

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,8 +1,8 @@
-number1=10
-number2=5
+number1 = 10 
+number2 = 5
 sum =number1 + number2
-sum =number1 - number2
-sum =number1 * number2
+sum1 =number1 - number2
+sum2 =number1 * number2
 print("Addition of", number1, "and", number2, "is", sum)
-print("Subtraction of", number1, "and", number2, "is", sum)
-print("Multiplication of", number1, "and", number2, "is", sum)
+print("Subtraction of", number1, "and", number2, "is", sum1)
+print("Multiplication of", number1, "and", number2, "is", sum2)
