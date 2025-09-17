912b76a3ad552d9618d56cc03ff5539e12068148
Sinethemba Zulu <sinethemba0363@gmail.com>
2025-09-17 12:58:51 +0200

    weather

---
+++ python_introduction/weather_advice.py
@@ -0,0 +1,11 @@
+User_weather = input("What's the weather like today? (sunny/rainy/cold):")
+if User_weather == "sunny":
+    print(" Wear a t-shirt and sunglasses.")
+
+elif User_weather == "rainy":
+    print("Don't forget your umbrella and a raincoat.")
+
+elif User_weather == "cold":
+    print("Make sure to wear a warm coat and a scarf.")
+else:
+    print("Sorry, I don't have recommendations for this weather")
