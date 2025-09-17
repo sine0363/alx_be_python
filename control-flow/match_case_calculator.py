06f5abc41ee9459e7baea0df761e73e8d5f34ffe
Sinethemba Zulu <sinethemba0363@gmail.com>
2025-09-17 14:33:05 +0200

    operators rectified file

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -2,7 +2,7 @@
 
 num1 = int(input("Enter the first number:"))
 num2 = int(input("Enter the second number:"))
-operator = input("Choose the operation (+, -, *, /)")
+operator = input("Choose the operation (+, -, *, /):")
 
 match operator:
     case "+":
