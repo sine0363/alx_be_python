58e7c5d4d0e78abd5a0080b9c041c18d3bd2e58b
Sinethemba Zulu <sinethemba0363@gmail.com>
2025-09-30 14:50:14 +0200

    changes6

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -11,7 +11,7 @@ def main():
     shopping_list = []
     while True:
         display_menu()
-        choice = input("Enter your choice: ")
+        choice = input("Enter your choice:")
         
 
         if choice == '1':
