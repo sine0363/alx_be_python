c34a385c33c2687d24e68bddf5baf5671524a526
Sinethemba Zulu <sinethemba0363@gmail.com>
2025-09-30 14:40:27 +0200

    changes3

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -3,9 +3,13 @@ FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 
 def  convert_to_celsius(fahrenheit):
-    Temperature = float (input("Enter the temperature to convert: "))
-    Temperature_conversion = (input("Is this temperature in Celsius or Fahrenheit? (C/F): "))
-    f_temp = (fahrenheit-32) *FAHRENHEIT_TO_CELSIUS_FACTOR
+    try:
+        Temperature =  input("Enter the temperature to convert: ")
+        number = float(Temperature)
+        Temperature_conversion = (input("Is this temperature in Celsius or Fahrenheit? (C/F): "))
+        f_temp = (fahrenheit-32) *FAHRENHEIT_TO_CELSIUS_FACTOR
+    except ValueError:
+        print("Value entered is not a number")
     print (f_temp)
 convert_to_celsius(12)
 
@@ -15,8 +19,14 @@ convert_to_celsius(12)
 
 
 def convert_to_farenheit(celsius):
-    Temperature = float(input("Enter the temperature to convert: "))
-    Temperature_conversion = input("Is this temperature in Celsius or Fahrenheit? (C/F): ")
-    c_temperature =  celsius*CELSIUS_TO_FAHRENHEIT_FACTOR +32
-    print(c_temperature)
+    try:
+        Temperature = float(input("Enter the temperature to convert: "))
+        number1= float(Temperature)
+        Temperature_conversion = input("Is this temperature in Celsius or Fahrenheit? (C/F): ")
+        c_temperature =  celsius*CELSIUS_TO_FAHRENHEIT_FACTOR +32
+        print(c_temperature)
+    except ValueError:
+        print("Value entered is not a number")
+
+        
 convert_to_farenheit(13)
