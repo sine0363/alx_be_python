228299e3357a2b3da5b7e54105fb1f41427798fe
Sinethemba Zulu <sinethemba0363@gmail.com>
2025-10-04 20:52:02 +0200

    change6

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,25 +1,23 @@
 class BankAccount:
     def __init__(self, account_balance=0):
-        self.__account_balance = account_balance
+        self.__account_balance = account_balance  # private attribute
 
     def deposit(self, amount):
         if amount > 0:
             self.__account_balance += amount
-            return amount  # return the deposited amount
+            return amount
         return 0
+        
 
     def withdraw(self, amount):
         if 0 < amount <= self.__account_balance:
             self.__account_balance -= amount
             return True
         return False
+            
 
     def display_balance(self):
-        return self.__account_balance
-
-    def get_balance(self):
-        return self.__account_balance
-
+        print(f"Current Balance: ${self.__account_balance}")
 
 
     
