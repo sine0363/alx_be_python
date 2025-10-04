2f4f4faf3885decde07fef329169bad899135b9c
Sinethemba Zulu <sinethemba0363@gmail.com>
2025-10-04 22:06:49 +0200

    changes1

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -3,16 +3,28 @@ def divide(x,y):
         if y==0:
             raise ZeroDivisionError("Error: Cannot divide by zero.")
         return x/y
+
+
 try:
     print(divide(6,2))
-    print(divide(6,0))
-    print(6,c)
-
-except ZeroDivisionError as e:
+except Exception as e:
      print(e)
 
+    
+try:
+     print(divide(6,0))
+
+except ZeroDivisionError as e:
+    print(e)
+
+try:
+    print(divide("ten", 5))
+
+
 except TypeError:
-     raise ValueError ("Error: Please enter numeric values only.")
+    print  ("Error: Please enter numeric values only.")
+
+
 
 
 
