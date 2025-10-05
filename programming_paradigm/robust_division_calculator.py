9257907be16c9215813089bcc6a7f4dc8ce99fb5
Sinethemba Zulu <sinethemba0363@gmail.com>
2025-10-05 21:09:29 +0200

    changes4

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -1,6 +1,5 @@
 def safe_divide(numerator, denominator):
     try:
-    
         numerator = float(numerator)
         denominator = float(denominator)
     except ValueError:
@@ -12,24 +11,5 @@ def safe_divide(numerator, denominator):
     return numerator / denominator
 
 
-# Test cases
-try:
-    print(f"The result of the division is {safe_divide(6, 2)}")
-  
-    
-  
-except Exception as e:
-    print(e)
 
-try:
-    print(safe_divide(6, 0))
-except ZeroDivisionError as e:
-    print(e)
-
-try:
-    print(safe_divide("ten", 5))
-except TypeError as e:
-    print(e)
-
-
- 
+print(f"The result of the division is {safe_divide(12, 2)}")
