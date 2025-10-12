b6c0421081cfbf6fa400bf9869a28c8010afbe73
Sinethemba Zulu <sinethemba0363@gmail.com>
2025-10-12 20:28:43 +0200

    changes

--- oop/book_class.py
+++ oop/book_class.py
@@ -8,12 +8,13 @@ class Book:
         print(f"Deleting {self.title}")
 
     def __str__(self):
-         return f"{self.title} by {self.author}, published in {self.year}"
+        return f" Book( {self.title} by {self.author}, published in {self.year})"
+
         
         
     def __repr__(self):
-        return f"Book('{self.title}', '{self.author}',  {self.year})"
-    
+        return f"{self.title}', '{self.author}', {self.year}"
+
        
         
        
